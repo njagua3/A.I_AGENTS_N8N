b4948cc3d3a05ce17c29c7f84829d4d9bea80fdc
njagua <njagua.gma@gmail.com>
2025-03-31 10:46:28 +0300

    assemble

---
+++ Notes.js
@@ -0,0 +1,27 @@
+/*
+BUILDING AI AGENTS
+
+INTRO TO AGENTIC WORKFLOWS IN N8N
+
+Whats an LLM(large language model)
+
+An llm generates text based on the input text.
+Its basically trying to guess the next word based on the input text
+
+In short its a function - input comes in output goes out
+
+An AI Agent builds on top of what an ai agent can do by adding goal oriented functionality
+
+It can leverage tools and other dependecies
+
+In short an LLM model takes some input text and outputs text 
+
+An agent can take input texts feed it to an LLM model, MAKE MULTIPLE thoughts under the hood
+
+Those thoughts can decide to use a tool and decidee next step make a decision and output that
+
+AI AGENT TOOLS
+      - Think of it as a fn an ai agent can call on to help complete a task it cant complete on its own
+      - Tools allow ai agents to interact with other systems gather data or take actions
+      
+*/
